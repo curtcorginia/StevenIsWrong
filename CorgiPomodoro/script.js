9b306f0ddb70dd8061a98032456c74fbcc8d9fc3
evansoohoo <evan.soohoo@gmail.com>
2019-06-09 15:59:07 -0700

    Revised getrandomdoggo api fetch to only retrieve corgis

--- CorgiPomodoro/script.js
+++ CorgiPomodoro/script.js
@@ -6,11 +6,64 @@ randomButton.addEventListener('click', getRandomDoggo);
 
 function getRandomDoggo(){
 	console.log("get random doggo");
-  fetch('https://dog.ceo/api/breeds/image/random')
+  //fetch('https://dog.ceo/api/breeds/image/random')
+  fetch('https://dog.ceo/api/breed/Pembroke/images/random')
     .then(checkStatus)
     .then(response => response.json())
     .then(data => handleData(data))
     .catch(error => notifyUser(error))
+}
+
+//populate List
+function populateList(){
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(checkStatus)
+    .then(response => response.json())
+    .then(data => createListItems(data.message))
+    .catch(error => notifyUser(error))
+}
+
+//handleData
+function handleData(data){
+  let url = data.message;
+  console.log(url)
+  let regex = /https:\/\/images\.dog\.ceo\/breeds\/(\w+\-?\w+)\/.+/g;
+  //let breedName = regex.exec(url);
+  let breedName = 'corgi';
+  //document.getElementById('randomImageContainer').innerHTML = `<img alt="random image of a ${fixBreed(breedName[1])}" src='${url}'/>`;
+  //document.querySelector('.dogInfo').innerHTML = `<p class="h5">Random image of a ${fixBreed(breedName[1])}</p>`;
+
+  document.getElementById('randomImageContainer').innerHTML = `<img alt="random image of a corgi" src='${url}'/>`;
+  document.querySelector('.dogInfo').innerHTML = `<p class="h5">Random image of a corgi</p>`;
+}
+
+//fixBreed
+function fixBreed(breedName){
+  
+  /*
+  if(breedName === 'germanshepherd'){
+    return 'German Shepherd';
+  }else if(breedName === 'mexicanhairless'){
+    return 'Mexican Hairless';
+  }else if(breedName === 'stbernard'){
+    return 'St. Bernard';
+  }else if(breedName === "african"){
+    return 'African Wild Dog';
+  }else if(breedName === 'bullterrier'){
+    return 'Bull Terier';
+  }
+
+  */
+  breedName === 'corgi';
+  return capitalize(breedName);
+}
+
+//capitalize breed name
+function capitalize(breedName){
+  return breedName.replace(/\-/g,' ')
+                  .split(" ")
+                  .map(word => word.charAt(0).toUpperCase()+word.slice(1))
+				          .join(" ");
 }
 
 //HELPER FUNCTIONS
